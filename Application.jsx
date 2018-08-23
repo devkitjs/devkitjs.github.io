1cf56c13ca286d04d31c7d207a7e5b26c73cfe17
Eduard <contactme@eduardmalakhov.com>
2018-08-23 13:33:53 +0300

    Draft dummy react application

---
+++ Application.jsx
@@ -0,0 +1,13 @@
+import React, { Component, ReactNode } from 'react';
+
+class Application extends Component {
+    render() {
+        return (
+            <div>
+                devkitjs react application
+            </div>
+        );
+    }
+}
+
+export default Application;
